8ca2f318d2fe92ce460c28bbed414d3c849f053d
Don Petersen <don@donpetersen.net>
2011-09-09 21:29:49 -0600

    Add nickname for client.  Better presentation all around.

--- socket_server/socket_server.rb
+++ socket_server/socket_server.rb
@@ -12,21 +12,35 @@ class Clients
     clients << client
   end
 
+  def find_by_socket(socket)
+    clients.detect { |c| c.socket == socket }
+  end
+
   def remove(socket)
-    client = clients.detect { |c| c.socket == socket }
-    clients.delete(client)
+    clients.delete(find_by_socket(socket))
   end
 
   def count
     clients.length
   end
 
+def system_broadcast(message)
+  broadcast "System: #{message}"
+end
+
+def client_broadcast(client, message)
+  broadcast "#{client.display_name}: #{message}"
+end
+
+protected
+
   def broadcast(message)
-    clients.each { |c| c.send "Broadcast: #{message}" }
+    clients.each { |c| c.send message }
   end
 end
 
 class Client
+  attr_accessor :nick
   attr_reader :socket
 
   def initialize(socket)
@@ -35,6 +49,10 @@ class Client
 
   def send(message)
     socket.send message
+  end
+
+  def display_name
+    nick || "Anonymous User"
   end
 end
 
@@ -45,18 +63,30 @@ EventMachine.run do
     ws.onopen do
       puts "Connected!"
       AllClients.add(Client.new(ws))
-      AllClients.broadcast "A client has joined.  Clients: #{AllClients.count}"
+      AllClients.system_broadcast "A client has joined.  Client Count: #{AllClients.count}"
     end
 
     ws.onclose do
       puts "Disconnected..."
+      client = AllClients.find_by_socket(ws)
       AllClients.remove(ws)
-      AllClients.broadcast "A client has disconnected. Clients: #{AllClients.count}"
+      AllClients.system_broadcast "#{client.display_name} has disconnected. Client Count: #{AllClients.count}"
     end
 
     ws.onmessage do |message|
       puts "Received Message: #{message}"
-      AllClients.broadcast message
+      if message[0] == "/"
+        if message =~ /^\/name (.*)$/
+          puts "Client wants to identify itself as #{$1}"
+          client = AllClients.find_by_socket(ws)
+          AllClients.system_broadcast "#{client.display_name} now known as #{$1}"
+          client.nick = $1
+        else
+          puts "I don't know how to deal with this command."
+        end
+      else
+        AllClients.client_broadcast AllClients.find_by_socket(ws), message
+      end
     end
   end
 end
