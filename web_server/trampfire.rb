35d365b97f0848506a141cbe9f83d226fe496d4a
Don Petersen <don@donpetersen.net>
2011-09-10 20:51:18 -0600

    Add ActiveRecord and a User model.

--- web_server/trampfire.rb
+++ web_server/trampfire.rb
@@ -1,6 +1,12 @@
+# This MUST go before sinatra/activerecord is required.  How the fuck
+# that thing actually works as documented... well, it doesn't.
+ENV["DATABASE_URL"] = "mysql2://localhost/trampfire_development"
+
 require 'pry'
 require 'sinatra'
+require 'sinatra/activerecord'
 require './lib/authorization_helpers'
+require './models/user'
 
 class TrampfireApp < Sinatra::Base
   set :public, 'public'
