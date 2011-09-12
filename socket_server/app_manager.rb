9097ce7716bd440575968346f38d13ae7df3e106
Don Petersen <don@donpetersen.net>
2011-09-11 19:05:07 -0600

    All named pipes are created by the owning applications.

--- socket_server/app_manager.rb
+++ socket_server/app_manager.rb
@@ -3,21 +3,29 @@ require 'pathname'
 class AppManager
   def initialize
     @apps = Pathname.glob("apps/*/").map { |i| i.basename.to_s }
-    @app_outgoing_pipe = open("apps/outgoing", "r+")
+    connect_incoming_named_pipe
   end
 
   def process(message)
     @apps.inject(message) do |message, app_directory|
-      app_incoming_pipe = open("apps/#{app_directory}/incoming", "w+")
-      app_incoming_pipe.puts message
-      app_incoming_pipe.flush
+      app_pipe = open("apps/#{app_directory}/incoming", "w+")
+      app_pipe.puts message
+      app_pipe.flush
 
-      @app_outgoing_pipe.gets
+      @incoming_pipe.gets
     end
   end
 
   def self.process(message)
     @instance ||= self.new
     @instance.process(message)
+  end
+
+protected
+
+  def connect_incoming_named_pipe
+    path = "apps/app_manager_incoming"
+    `mkfifo #{path}` unless File.exist?(path)
+    @incoming_pipe = open(path, "r+")
   end
 end
--- socket_server/apps/app_base.rb
+++ socket_server/apps/app_base.rb
@@ -1,9 +1,26 @@
 class AppBase
   def initialize
-    @incoming_pipe = open("incoming", "r+")
-    @outgoing_pipe = open("../outgoing", "w+")
+    connect_incoming_pipe
 
     wait_for_incoming
+  end
+
+protected
+
+  def connect_incoming_pipe
+    path = "incoming"
+    `mkfifo #{path}` unless File.exist?(path)
+    @incoming_pipe = open("incoming", "r+")
+  end
+
+  # Called at message time, not on initialize.  If the pipe doesn't
+  # exist, Ruby creates a file in its place.  On first launch, it
+  # probably won't be there by the time this initializes.
+  def connect_outgoing_pipe
+    return if @outgoing_pipe
+
+    path = "../app_manager_incoming"
+    @outgoing_pipe = open(path, "w+") if File.exist?(path)
   end
 
   def wait_for_incoming
@@ -14,6 +31,7 @@ class AppBase
     message = process(message)
 
     puts "Message modded to: #{message}"
+    connect_outgoing_pipe
     @outgoing_pipe.puts message
     @outgoing_pipe.flush
 
