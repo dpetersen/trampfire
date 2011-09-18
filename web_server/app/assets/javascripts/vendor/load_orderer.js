6dec822728ee694a9b53a5432068844904408629
Don Petersen <don@donpetersen.net>
2011-09-18 11:03:46 -0600

    Replaced minified JS/CSS libraries with dev versions.

--- web_server/app/assets/javascripts/vendor/load_orderer.js
+++ web_server/app/assets/javascripts/vendor/load_orderer.js
@@ -1,7 +1,7 @@
 // These files must be loaded in a specific order.
 //
 //= require vendor/json2
-//= require vendor/jquery-1.6.3.min
-//= require vendor/jquery.ba-resize.min
+//= require vendor/jquery-1.6.4
+//= require vendor/jquery.ba-resize
 //= require vendor/underscore
 //= require vendor/backbone
