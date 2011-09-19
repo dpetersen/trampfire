b4e4d53f30d7f930106558f394ce059f3a9ee812
Don Petersen <don@donpetersen.net>
2011-09-18 21:28:05 -0600

    Refactor Github bot for a bit of clarity.

--- socket_server/bots/github/github_bot.rb
+++ socket_server/bots/github/github_bot.rb
@@ -8,25 +8,15 @@ require 'pry'
 class GithubBotRequest < BotRequestBase
   def process
     if message =~ /^http(?:s)?:\/\/(?:www\.)?github\.com\/([^\/]+)\/([^\/]+)\/commit\/([a-f0-9]{40})$/
-      @user = $1
-      @repo = $2
-      @sha = $3
+      user = $1
+      repo = $2
+      sha = $3
 
       within_subprocess do
-        github =  GithubApiHelper.new(config["username"], config["api_key"])
-        repo = github.repo(@user, @repo)
-        commit = github.commit(@user, @repo, @sha)
-        md5 = Digest::MD5.hexdigest(commit["author"]["email"])
-        gravatar_url = "http://gravatar.com/avatar/#{md5}"
+        view_hash = build_view_hash_for_commit(user, repo, sha)
+        html = render_view("commit", view_hash)
 
-        message_hash["data"] = render_view "commit", {
-          repository_name: repo["name"],
-          repo_url: repo["url"],
-          commit_author: commit["author"]["name"],
-          gravatar_url: gravatar_url,
-          commit_url: "http://github.com#{commit["url"]}",
-          commit_message: commit["message"]
-        }
+        message_hash["data"] = html
         @asynchronous_pipe.puts message_hash.to_json
         @asynchronous_pipe.flush
       end
@@ -40,6 +30,23 @@ protected
   def octocatize_message(message)
     octocat_image_url = "http://th00.deviantart.net/fs70/150/i/2011/178/a/f/octocat_by_rstovall-d3k6a7n.jpg"
     %{<a href="#{message}"><img src="#{octocat_image_url}" />#{message}</a>}
+  end
+
+  def build_view_hash_for_commit(user_name, repo_name, sha)
+    github =  GithubApiHelper.new(config["username"], config["api_key"])
+    repo = github.repo(user_name, repo_name)
+    commit = github.commit(user_name, repo_name, sha)
+    md5 = Digest::MD5.hexdigest(commit["author"]["email"])
+    gravatar_url = "http://gravatar.com/avatar/#{md5}"
+
+    {
+      repository_name: repo["name"],
+      repo_url: repo["url"],
+      commit_author: commit["author"]["name"],
+      gravatar_url: gravatar_url,
+      commit_url: "http://github.com#{commit["url"]}",
+      commit_message: commit["message"]
+    }
   end
 end
 
