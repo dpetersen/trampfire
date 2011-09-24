bfdee856db33b26e97fb146811dca2af98139d01
Don Petersen <don@donpetersen.net>
2011-09-23 18:49:28 -0600

    Don't process post-commit hooks for unknown repos.

--- socket_server/bots/github/github_bot.rb
+++ socket_server/bots/github/github_bot.rb
@@ -16,21 +16,24 @@ class GithubBotRequest < BotRequestBase
       repository_owner = post_commit_object["repository"]["owner"]["name"]
       repository_name = post_commit_object["repository"]["name"]
 
-      message_html = "<h4>Changes have been pushed to '#{repository_name}'</h4>"
+      if RepositoryWatch.where(owner_login: repository_owner, repository_name: repository_name).exists?
+        message_html = "<h4>Changes have been pushed to '#{repository_name}'</h4>"
 
-      post_commit_object["commits"].each do |commit_object|
-        sha = commit_object["id"]
-        api =  GithubApiHelper.new(config["username"], config["api_key"])
-        commit = api.commit(repository_owner, repository_name, sha)
+        post_commit_object["commits"].each do |commit_object|
+          sha = commit_object["id"]
+          api =  GithubApiHelper.new(config["username"], config["api_key"])
+          commit = api.commit(repository_owner, repository_name, sha)
 
-        view_hash = build_view_hash_for_commit(repository_owner, repository_name, sha)
-        message_html << render_view("commit", view_hash)
+          view_hash = build_view_hash_for_commit(repository_owner, repository_name, sha)
+          message_html << render_view("commit", view_hash)
+        end
+
+        message_hash["data"] = message_html
+        interprocess_message = BotInitiatedInterprocessMessage.new("GitHubBot", "post_commit", message_hash: message_hash)
+        @asynchronous_pipe.puts interprocess_message.to_json
+        @asynchronous_pipe.flush
+      else raise "Got a post-commit hook from unknown repo: '#{repository_owner}/#{repository_name}'"
       end
-
-      message_hash["data"] = message_html
-      interprocess_message = BotInitiatedInterprocessMessage.new("GitHubBot", "post_commit", message_hash: message_hash)
-      @asynchronous_pipe.puts interprocess_message.to_json
-      @asynchronous_pipe.flush
     end
   end
 
